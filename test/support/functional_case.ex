ed8f605ce26f2e4da6bfc761b34d7780fa05165b
pmeinhardt <mnhrdt@gmail.com>
2017-01-25 23:38:39 +0100

    Run sshd inside docker containers for testing
    - Provide helpers for working with docker (wrap command-line)
    - Containers are created on test setup, killed on test exit

--- test/support/functional_case.ex
+++ test/support/functional_case.ex
@@ -4,12 +4,28 @@ defmodule SSHKit.FunctionalCase do
   setup tags do
     # IO.inspect(Map.get(tags, :servers, 1))
 
-    # start docker container(s)
+    cmd = "/usr/sbin/sshd"
+    args = ["-D"]
+
+    # For debugging, let sshd be more verbose:
+    # args = ["-D", "-d", "-d", "-d"]
+
+    id = Docker.run!(["--rm", "--detach", "--publish-all"], "sshkit-test-sshd", cmd, args)
+
     # set up container(s) with user(s) & key(s)
+    addr = Docker.cmd!("port", [id, "22/tcp"])
+    uri = URI.parse("tcp://" <> addr)
+
+    IO.puts(id)
+    IO.puts(uri.port)
+    IO.puts(Docker.exec!(id, "whoami"))
 
     on_exit fn ->
-      :ok # ensure containers are killed and removed
+      Docker.kill!([id])
+      :ok
     end
+
+    # expose host objects with port, user, key information
 
     :ok
   end
