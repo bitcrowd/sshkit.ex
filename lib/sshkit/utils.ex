88a653c8a511c611297b4ce1124ff2587ea0de17
Sasha Voynow <sasha@labzero.com>
2017-03-10 12:43:17 -0800

    Convert binary parameter and options to charlists

--- lib/sshkit/utils.ex
+++ lib/sshkit/utils.ex
@@ -2,4 +2,11 @@ defmodule SSHKit.Utils do
   def shellescape(value), do: value
 
   def shellquote(value), do: value
+
+  def strings_to_charlists(opts) when is_list(opts) do
+    Enum.map(opts, fn {k, v} -> {k, charlist(v)} end)    
+  end
+
+  defp charlist(value) when is_binary(value), do: to_charlist(value)
+  defp charlist(value), do: value
 end
