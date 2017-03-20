6d6f0d8af64fc97fc3e9bfe519ae0fd10f6c2eb0
pmeinhardt <mnhrdt@gmail.com>
2017-03-20 17:19:11 +0100

    [#15] Add code style checks

--- mix.exs
+++ mix.exs
@@ -23,8 +23,9 @@ defmodule SSHKit.Mixfile do
   end
 
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev},
-     {:inch_ex, ">= 0.0.0", only: :dev}]
+    [{:credo, "~> 0.7", only: [:dev, :test]},
+     {:ex_doc, "~> 0.14", only: [:dev]},
+     {:inch_ex, ">= 0.0.0", only: [:dev]}]
   end
 
   defp description do
