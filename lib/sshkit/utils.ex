bf3bfe1803165b0d18e31759ba13e2f94db9a901
pmeinhardt <mnhrdt@gmail.com>
2017-03-21 14:30:31 +0100

    [#9] Convert binaries to charlists for Erlang

--- lib/sshkit/utils.ex
+++ lib/sshkit/utils.ex
@@ -2,4 +2,13 @@ defmodule SSHKit.Utils do
   def shellescape(value), do: value
 
   def shellquote(value), do: value
+
+  def charlistify(value) do
+    cond do
+      Keyword.keyword?(value) -> Enum.map(value, fn {k, v} -> {k, charlistify(v)} end)
+      is_list(value) -> Enum.map(value, &charlistify/1)
+      is_binary(value) -> to_charlist(value)
+      true -> value
+    end
+  end
 end
---
+++ test/sshkit/utils_test.exs
@@ -0,0 +1,32 @@
+defmodule SSHKit.UtilsTest do
+  use ExUnit.Case
+
+  alias SSHKit.Utils
+
+  describe "charlistify/1" do
+    test "converts binaries to char lists" do
+      assert Utils.charlistify("sshkit") == 'sshkit'
+    end
+
+    test "converts binaries in lists" do
+      assert Utils.charlistify(["ssh-rsa","ssh-dss"]) == ['ssh-rsa','ssh-dss']
+    end
+
+    test "converts binaries in keywords" do
+      assert Utils.charlistify([inet: :inet6, user: "me"]) == [inet: :inet6, user: 'me']
+    end
+
+    test "converts binaries in nested lists" do
+      actual = Utils.charlistify([pref_public_key_algs: ["ssh-rsa", "ssh-dss"]])
+      expected = [pref_public_key_algs: ['ssh-rsa','ssh-dss']]
+      assert actual == expected
+    end
+
+    test "converts binaries in nested keywords" do
+      ciphers = [client2server: ["aes128-ctr"], server2client: ["aes128-cbc", "3des-cbc"]]
+      actual = Utils.charlistify([preferred_algorithms: [cipher: ciphers]])
+      expected = [preferred_algorithms: [cipher: Utils.charlistify(ciphers)]]
+      assert actual == expected
+    end
+  end
+end
