ff43e4b577c2aa22d9911b349572753933b85d92
pmeinhardt <mnhrdt@gmail.com>
2017-03-22 08:00:46 +0100

    Use binaries in SCP.Command and add tests

--- lib/sshkit/scp/command.ex
+++ lib/sshkit/scp/command.ex
@@ -3,18 +3,18 @@ defmodule SSHKit.SCP.Command do
 
   import SSHKit.Utils
 
-  @flags [verbose: '-v', preserve: '-p', recursive: '-r']
+  @flags [verbose: "-v", preserve: "-p", recursive: "-r"]
 
   def build(:upload, path, options) do
-    scp('-t', path, options)
+    scp("-t", path, options)
   end
 
   def build(:download, path, options) do
-    scp('-f', path, options)
+    scp("-f", path, options)
   end
 
   defp scp(mode, path, options) do
-    'scp #{mode}' |> flag(options) |> at(path)
+    "scp #{mode}" |> flag(options) |> at(path)
   end
 
   defp flag(command, options) do
@@ -26,6 +26,6 @@ defmodule SSHKit.SCP.Command do
   end
 
   defp at(command, path) do
-    '#{command} #{shellescape(path)}'
+    "#{command} #{shellescape(path)}"
   end
 end
