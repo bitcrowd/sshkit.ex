b29689b89dd20cfa5420db8c5240ce282932ec80
Philipp Tessenow <philipp@tessenow.org>
2017-05-03 15:32:29 +0200

    finished testing the basic sshkit dsl

--- test/sshkit_functional_test.exs
+++ test/sshkit_functional_test.exs
@@ -1,6 +1,7 @@
 defmodule SSHKitFunctionalTest do
+  import SSHKit.FunctionalCaseHelpers
+
   use SSHKit.FunctionalCase, async: true
-  # doctest SSHKit
 
   @defaults [silently_accept_hosts: true]
 
@@ -19,40 +20,47 @@ defmodule SSHKitFunctionalTest do
     })
   end
 
+  def stdout(output) do
+    output
+    |> Keyword.get_values(:normal)
+    |> Enum.join()
+  end
+
+  def stderr(output) do
+    output
+    |> Keyword.get_values(:stderr)
+    |> Enum.join()
+  end
+
   @tag boot: 1
   test "connects", %{hosts: [host]} do
     [{:ok, output, 0}] = SSHKit.run(build_context(host), "whoami")
 
-    name =
-      output
-      |> Keyword.get_values(:normal)
-      |> Enum.join()
-      |> String.trim()
-
+    name = String.trim(stdout(output))
     assert name == host.user
   end
 
   @tag boot: 1
   test "run", %{hosts: [host]} do
-    context =
-      build_context(host)
-      |> SSHKit.pwd("/var/log")
+    context = build_context(host)
 
     [{:ok, output, status}] = SSHKit.run(context, "pwd")
     assert status == 0
-    assert output == [normal: "/var/log\n"]
+    output = stdout(output)
+    assert output == "/home/me\n"
 
     [{:ok, output, status}] = SSHKit.run(context, "ls non-existing")
     assert status == 1
-    [stderr: stderr] = output
-    assert stderr =~ "ls: non-existing: No such file or directory"
+    output = stderr(output)
+    assert output =~ "ls: non-existing: No such file or directory"
 
     [{:ok, output, status}] = SSHKit.run(context, "does-not-exist")
     assert status == 127
-    [stderr: stderr] = output
-    assert stderr =~ "'does-not-exist': No such file or directory"
+    output = stderr(output)
+    assert output =~ "'does-not-exist': No such file or directory"
   end
 
+  @tag :skip # not yet implemented
   @tag boot: 1
   test "env", %{hosts: [host]} do
     [{:ok, output, status}] =
@@ -62,9 +70,9 @@ defmodule SSHKitFunctionalTest do
       |> SSHKit.run("env")
 
     assert status == 0
-    [normal: stdout] = output
-    assert stdout =~ "NODE_ENV=production"
-    assert stdout =~ ~r/PATH=.*\/\.rbenv\/shims:/
+    output = stdout(output)
+    assert output =~ "NODE_ENV=production"
+    assert output =~ ~r/PATH=.*\/\.rbenv\/shims:/
   end
 
   @tag boot: 1
@@ -73,14 +81,64 @@ defmodule SSHKitFunctionalTest do
               |> SSHKit.umask("077")
     SSHKit.run(context, "mkdir my_dir")
     SSHKit.run(context, "touch my_file")
-    [{:ok, output, status}] = SSHKit.run(context, "ls -ld my_dir my_file")
-    IO.inspect(SSHKit.Context.build(context, "mkdir bla"))
+    [{:ok, output, status}] = SSHKit.run(context, "ls -la")
 
     assert status == 0
-    # drwx------ 2 vivek vivek 4096 2011-03-04 02:05 dir1
-    # -rw------- 1 vivek vivek    0 2011-03-04 02:05 file
-    # drwx--S--- 2 me    me    4096 May  2 21:26 my_dir
-    # -rw------- 1 me    me       0 May  2 21:26 my_file
-    assert output == [normal: "/var/log\n"]
+    output = stdout(output)
+    assert output =~ ~r/drwx--S---\s+2\s+me\s+me\s+4096.+my_dir/
+    assert output =~ ~r/-rw-------\s+1\s+me\s+me\s+0.+my_file/
+  end
+
+  @tag boot: 1
+  test "pwd", %{hosts: [host]} do
+    context = build_context(host)
+              |> SSHKit.pwd("/var/log")
+
+    [{:ok, output, status}] = SSHKit.run(context, "pwd")
+    assert status == 0
+    output = stdout(output)
+    assert output == "/var/log\n"
+  end
+
+  @tag skip: true # it produces an error: "sudo not found" on stderr
+  @tag boot: 1
+  test "user", %{hosts: [host]} do
+    adduser(host, "despicable_me")
+
+    context = build_context(host)
+              |> SSHKit.user("despicable_me")
+
+    [{:ok, output, status}] = SSHKit.run(context, "whoami")
+    output = stdout(output)
+    assert output == "despicable_me\n"
+    assert status == 0
+  end
+
+  @tag skip: true # "sh: sudo: not found" on stderr
+  @tag boot: 1
+  test "group", %{hosts: [host]} do
+    adduser(host, "gru")
+    addgroup(host, "villains")
+    addgroup(host, "minion_owners")
+    add_user_to_group(host, "gru", "villains")
+    add_user_to_group(host, "gru", "minion_owners")
+
+    context = build_context(host)
+              |> SSHKit.user("gru")
+              |> SSHKit.group("villains")
+
+    [{:ok, output, status}] = SSHKit.run(context, "id -gn gru")
+    output = stdout(output)
+    assert output == "villains\n"
+    assert status == 0
+
+    context = build_context(host)
+              |> SSHKit.user("gru")
+              |> SSHKit.group("minion_owners")
+
+    [{:ok, output, status}] = SSHKit.run(context, "id -gn gru")
+    output = stdout(output)
+    assert output == "minion_owners\n"
+    assert status == 0
   end
 end
