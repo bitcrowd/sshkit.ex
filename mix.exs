e533da8416f17d272ca72f41910c2e8fc3e8f921
Brien Wankel <brien@reebosak.net>
2017-06-29 09:30:58 -0700

    Downgrade Elixir version back to 1.3

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule SSHKit.Mixfile do
     [app: :sshkit,
      name: "sshkit",
      version: @version,
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
