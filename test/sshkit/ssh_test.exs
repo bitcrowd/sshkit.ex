dd5c18dd0737fdd8e2b37f058c333f026467f3f1
Max Mulatz <klappradla@gmail.com>
2017-07-05 10:54:44 +0200

    Add unit tests for SSHKit.SSH.connect/2
    The tests use sandbox ssh-modules, therefore it's not possible to test
    the simplest function call (connect with no options), since using
    sandbox mode already is an "option".
    
    The test-only SandboxError module now also provides close/1, in order to
    make sure it is NOT called.

--- test/sshkit/ssh_test.exs
+++ test/sshkit/ssh_test.exs
@@ -13,35 +13,75 @@ defmodule SSHKit.SSHTest do
 
   defmodule SSHSandboxError do
     def connect(_, _, _, _), do: {:error, :sandbox}
+
+    def close(_) do
+      send self(), :closed_sandbox_connection
+      :ok
+    end
+  end
+
+  setup_all do
+    {:ok, host: "foo.io"}
+  end
+
+  describe "connect/2" do
+    @options [ssh_modules: %{ssh: SSHSandboxSuccess, ssh_connection: :ssh_connection}]
+    test "open sandbox connection with given options and keep it open", state do
+      user = "test"
+      options = @options ++ [user: user]
+      conn = %SSHKit.SSH.Connection{
+        host:        String.to_charlist(state[:host]),
+        options:     [user_interaction: false, user: String.to_charlist(user)],
+        port:        22,
+        ref:         :sandbox,
+        ssh_modules: Keyword.get(options, :ssh_modules)
+      }
+
+      assert connect(state[:host], options) == {:ok, conn}
+      refute_received :closed_sandbox_connection
+    end
+
+    @options [ssh_modules: %{ssh: SSHSandboxError, ssh_connection: :ssh_connection}]
+    test "return error and do not attempt to close if connection cannot be opened", state do
+      assert connect(state[:host], @options) == {:error, :sandbox}
+      refute_received :closed_sandbox_connection
+    end
+
+    test "return error and do not attempt to close if no host given" do
+      assert connect(nil, @options) == {:error, "No host given."}
+      refute_received :closed_sandbox_connection
+    end
+
+    test "error if options not provided as List", state do
+      options = %{user: "me", password: "secret"}
+      assert_raise FunctionClauseError, fn -> connect(state[:host], options) end
+    end
   end
 
   describe "connect/3" do
     @options [ssh_modules: %{ssh: SSHSandboxSuccess, ssh_connection: :ssh_connection}]
-    test "execute function on open connection" do
-      host = "foo"
+    test "execute function on open connection", state do
       func = fn(conn) ->
         assert conn.ssh_modules == Keyword.get(@options, :ssh_modules)
         42
       end
 
-      assert connect(host, @options, func) == {:ok, 42}
+      assert connect(state[:host], @options, func) == {:ok, 42}
       assert_received :closed_sandbox_connection
     end
 
-    test "close connection although function errored" do
-      host = "foo"
+    test "close connection although function errored", state do
       func = fn(_conn) -> raise("error") end
 
-      assert_raise RuntimeError, "error", fn -> connect(host, @options, func) end
+      assert_raise RuntimeError, "error", fn -> connect(state[:host], @options, func) end
       assert_received :closed_sandbox_connection
     end
 
     @options [ssh_modules: %{ssh: SSHSandboxError, ssh_connection: :ssh_connection}]
-    test "error during connect" do
-      host = "foo"
+    test "error during connect", state do
       func = fn(_conn) -> flunk "should never be called" end
 
-      assert connect(host, @options, func) == {:error, :sandbox}
+      assert connect(state[:host], @options, func) == {:error, :sandbox}
       refute_received :closed_sandbox_connection
     end
   end
