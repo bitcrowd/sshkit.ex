41c7371c09418be6ab78a67da66edfd98dfc9af9
pmeinhardt <mnhrdt@gmail.com>
2018-02-16 03:13:10 +0100

    Add tests for `Context.build/2`

--- test/sshkit/context_test.exs
+++ test/sshkit/context_test.exs
@@ -6,25 +6,41 @@ defmodule SSHKit.ContextTest do
   @empty %Context{hosts: []}
 
   describe "build/2" do
-    test "build with path" do
+    test "with user" do
       command =
-        %Context{@empty | path: "/var/www"}
-        |> Context.build("ls -l")
+        %Context{@empty | user: "me"}
+        |> Context.build("whoami")
 
-      assert command == "cd /var/www && /usr/bin/env ls -l"
+      assert command == "sudo -n -u me -- sh -c '/usr/bin/env whoami'"
     end
 
-    test "build with env" do
+    test "with group" do
+      command =
+        %Context{@empty | group: "crew"}
+        |> Context.build("id -g -n")
+
+      assert command == "sudo -n -g crew -- sh -c '/usr/bin/env id -g -n'"
+    end
+
+    test "with user and group" do
+      command =
+        %Context{@empty | user: "me", group: "crew"}
+        |> Context.build("id")
+
+      assert command == "sudo -n -u me -g crew -- sh -c '/usr/bin/env id'"
+    end
+
+    test "with env" do
       command =
         %Context{@empty | env: %{"NODE_ENV" => "production", "CI" => "true"}}
         |> Context.build("echo \"hello\"")
 
-      assert command =~ ~r/export/
+      assert command =~ ~r/\A\(export .+ && \/usr\/bin\/env echo "hello"\)\z/
       assert command =~ ~r/NODE_ENV="production"/
       assert command =~ ~r/CI="true"/
     end
 
-    test "build with env including whitespace" do
+    test "with env including whitespace" do
       command =
         %Context{@empty | env: %{"CUSTOM" => "env variable"}}
         |> Context.build("echo \"hello\"")
@@ -32,21 +48,36 @@ defmodule SSHKit.ContextTest do
       assert command =~ ~r/export CUSTOM="env variable"/
     end
 
-    test "build with nil env" do
+    test "with nil env" do
       command =
         %Context{@empty | env: nil}
         |> Context.build("ls")
 
-
       assert command == "/usr/bin/env ls"
     end
 
-    test "build with empty env" do
+    test "with empty env" do
       command =
         %Context{@empty | env: %{}}
         |> Context.build("ls")
 
       assert command == "/usr/bin/env ls"
+    end
+
+    test "with umask" do
+      command =
+        %Context{@empty | umask: "077"}
+        |> Context.build("touch precious.txt")
+
+      assert command == "umask 077 && /usr/bin/env touch precious.txt"
+    end
+
+    test "with path" do
+      command =
+        %Context{@empty | path: "/var/www"}
+        |> Context.build("ls -l")
+
+      assert command == "cd /var/www && /usr/bin/env ls -l"
     end
   end
 end
