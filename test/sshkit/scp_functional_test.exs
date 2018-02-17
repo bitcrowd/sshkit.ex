4edfdeef3f87c3449dcd90959ce87bedc10a86dd
pmeinhardt <mnhrdt@gmail.com>
2018-02-17 21:39:09 +0100

    Reorganize fixture files a bit
    Use clear names to indicate whether files start out on the "local" or
    the "remote" (Docker container running SSH server) file system.
    
    Remove unused directory level in the local fixture files.

--- test/sshkit/scp_functional_test.exs
+++ test/sshkit/scp_functional_test.exs
@@ -11,7 +11,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "sends a file", %{hosts: [host]} do
       options = @defaults ++ [port: host.port, user: host.user, password: host.password]
-      local = "test/fixtures/local_workspace/local_file.txt"
+      local = "test/fixtures/local.txt"
       remote = "file.txt"
 
       SSH.connect host.ip, options, fn(conn) ->
@@ -23,7 +23,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "recursive: true", %{hosts: [host]} do
       options = @defaults ++ [port: host.port, user: host.user, password: host.password]
-      local = "test/fixtures/local_workspace"
+      local = "test/fixtures"
       remote = "/home/#{host.user}/destination"
 
       SSH.connect host.ip, options, fn(conn) ->
@@ -35,7 +35,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "preserve: true", %{hosts: [host]} do
       options = @defaults ++ [port: host.port, user: host.user, password: host.password]
-      local = "test/fixtures/local_workspace/local_file.txt"
+      local = "test/fixtures/local.txt"
       remote = "file.txt"
 
       SSH.connect host.ip, options, fn(conn) ->
@@ -48,7 +48,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "recursive: true, preserve: true", %{hosts: [host]} do
       options = [port: host.port, user: host.user, password: host.password]
-      local = "test/fixtures/local_workspace/"
+      local = "test/fixtures/"
       remote = "/home/#{host.user}/destination"
 
       SSH.connect host.ip, options, fn(conn) ->
@@ -63,7 +63,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "gets a file", %{hosts: [host]} do
       options = @defaults ++ [port: host.port, user: host.user, password: host.password]
-      remote = "/fixtures/file.txt"
+      remote = "/fixtures/remote.txt"
       local = create_local_tmp_path()
       on_exit fn -> File.rm(local) end
 
@@ -89,7 +89,7 @@ defmodule SSHKit.SCPFunctionalTest do
     @tag boot: 1
     test "preserve: true", %{hosts: [host]} do
       options = @defaults ++ [port: host.port, user: host.user, password: host.password]
-      remote = "/fixtures/file.txt"
+      remote = "/fixtures/remote.txt"
       local = create_local_tmp_path()
       on_exit fn -> File.rm(local) end
 
