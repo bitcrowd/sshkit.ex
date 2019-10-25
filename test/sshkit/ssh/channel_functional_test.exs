43f30640aaccb5e7e405d9bb97881620d99873d1
Bart van Zon <bart@bartvanzon.com>
2019-10-25 13:22:56 +0200

    Fix message retrieval in functional channel test
    When retrieving the messages using Channel.recv it can contain anything
    form a partial message to multiple messages. This would occasionally
    cause recv to wait for a message that we already received earlier.

--- test/sshkit/ssh/channel_functional_test.exs
+++ test/sshkit/ssh/channel_functional_test.exs
@@ -9,15 +9,25 @@ defmodule SSHKit.SSH.ChannelFunctionalTest do
     @tag boot: [@bootconf]
     test "with user", %{hosts: [host]} do
       {:ok, conn} = SSHKit.SSH.connect(host.name, host.options)
-
       {:ok, channel} = SSHKit.SSH.Channel.open(conn)
       :success = SSHKit.SSH.Channel.subsystem(channel, "greeting-subsystem")
-      SSHKit.SSH.Channel.send(channel, "Lorem\n")
-      {:ok, {:data, _channel, _type, welcome_message}} = SSHKit.SSH.Channel.recv(channel)
-      {:ok, {:data, _channel, _type, response_message}} = SSHKit.SSH.Channel.recv(channel)
 
-      assert welcome_message == "Hello, who am I talking to?\n"
-      assert response_message == "It's nice to meet you Lorem\n"
+      SSHKit.SSH.Channel.send(channel, "Lorem\n")
+
+      [welcome_message, response_message, _] = get_messages(channel)
+
+      assert welcome_message == "Hello, who am I talking to?"
+      assert response_message == "It's nice to meet you Lorem"
+    end
+  end
+
+  defp get_messages(channel, message \\ "") do
+    {:ok, {:data, _channel, _type, next_line}} = SSHKit.SSH.Channel.recv(channel)
+
+    if String.ends_with?(next_line, "Lorem\n") do
+      String.split("#{message}#{next_line}", "\n")
+    else
+      get_messages(channel, "#{message}#{next_line}")
     end
   end
 end
